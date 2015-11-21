8b94e7b381fd14eb7ea439f431acf6a53cfc0018
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2015-11-21 16:05:49 -0700

    added additional properties to Memory

--- app/src/main/java/kylefrisbie/com/memorymap/Memory.java
+++ app/src/main/java/kylefrisbie/com/memorymap/Memory.java
@@ -12,8 +12,20 @@ public class Memory extends SugarRecord<Memory> {
     String title;
     Date date;
     ArrayList<String> people;
+    String photoURI;
+    String description;
+    double[] location;
 
     public Memory() {}
 
-    
+
+    public Memory(String title, Date date, ArrayList<String> people,
+                  String photoURI, String description, double[] location) {
+        this.title = title;
+        this.date = date;
+        this.people = people;
+        this.photoURI = photoURI;
+        this.description = description;
+        this.location = location;
+    }
 }
